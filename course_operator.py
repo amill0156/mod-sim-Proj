dd65f0f3aabd476b11298f371f96f2fda16925dc
amill0156 <ashton.miller82@gmail.com>
2024-10-01 17:59:04 -0400

    modified mod_sim.py and course_operator.py

---
+++ course_operator.py
@@ -0,0 +1,14 @@
+import golf_course
+
+class CourseOperator:
+    def __init__(self, cart_condition, need_marshall, group_status):
+        self.cart_condition = cart_condition
+        self.need_marshall = need_marshall
+        self.group_status = group_status
+
+    #def detect_slow(self):
+
+
+    #def send_marshall(self):
+
+
--- mod_sim.py
+++ mod_sim.py
@@ -1,6 +1,7 @@
 import simpy
 import random
-
+import golf_course
+import course_operator
 
 def main():
     print("Hello and welcome to Miller's Landing Golf and Country Club!")
@@ -35,6 +36,19 @@ def cart_recieve():
             break
 
 
+def cart_return():
+    carts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+             30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52]
+    taken_carts = []
+    tee_times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]
+
+    for group_num in tee_times:
+            carts.add(taken_carts[0])
+            carts.add(taken_carts[1])
+            print("Carts taken:")
+            print(str(taken_carts))
+            print("Carts available:")
+            print(carts)
 
 
 
