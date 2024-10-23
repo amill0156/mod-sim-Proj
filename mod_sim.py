b5bc976b7510cc480900e818411260cd7516c094
amill0156 <ashton.miller82@gmail.com>
2024-10-23 17:36:04 -0400

    changes made to mod_sim.py to reflect a recyling of carts for new groups: in work (possibly may be scrapped)

--- mod_sim.py
+++ mod_sim.py
@@ -39,42 +39,60 @@ class ModSim:
         else:  # Par 5 holes (holes 1, 8, 15, 18)
             return 18
 
-    def play_hole(self, env, cart, hole_number, wait_time):
+    def play_hole(self, env, cart, group_num, hole_number, wait_time):
         standard_time = self.get_standard_time(hole_number)
-        print(f"Cart {cart} is starting hole {hole_number} at time {env.now}. Par wait time: {wait_time} minutes.")
+        print(f"Cart {cart} is starting hole {hole_number} at time {env.now}. Time taken at hole: {wait_time} minutes.")
         yield env.timeout(wait_time)  # Simulate time taken to complete the hole
         print(f"Cart {cart} has finished hole {hole_number} at time {env.now} after {wait_time} minutes.")
 
         actual_wait_time = wait_time
-        self.course_operator.detect_slow(cart, hole_number, actual_wait_time, standard_time)
+        self.course_operator.detect_slow(group_num, hole_number, actual_wait_time, standard_time)
 
-    def send_cart_to_course(self, env, cart, wait_times):
+    def send_cart_to_course(self, env, cart, group_num, wait_times):
         for hole_number in range(1, 19):  # Loop through all 18 holes
-            yield env.process(self.play_hole(env, cart, hole_number, wait_times[hole_number-1]))  # Process each hole one by one
+            yield env.process(self.play_hole(env, cart, group_num, hole_number, wait_times[hole_number-1]))  # Process each hole one by one
 
-        print(f"Group {cart} has completed the course at time {env.now}.")
+        print(f"Cart {cart} has completed the course at time {env.now}.")
+        #yield env.process(self.cart_return(env))
+
 
     def cart_recieve(self, env):
-        tee_times = list(range(1, 14))
+        tee_times = list(range(1, 27))
         group_start_delay = 10  # Delay of 10 time units between each group
 
         for group_num in tee_times:
             wait_times = [self.get_par_wait_time(hole_number) for hole_number in range(1, 19)]
-            print(f"\nGroup {group_num} is about to go off at time {env.now}...")
+            print(f"Group {group_num} is about to go off at time {env.now}...")
             carts_to_take = clubhouse.carts[:2]
             for cart in carts_to_take:
                 clubhouse.remove_cart(cart)
-                env.process(self.send_cart_to_course(env, cart, wait_times))  # Start each cart process concurrently
+                env.process(self.send_cart_to_course(env, cart, group_num, wait_times))  # Start each cart process concurrently
 
-            yield env.timeout(group_start_delay)  # Stagger group starts
+            #yield env.timeout(group_start_delay)  # Stagger group starts
 
             #print(f"Group {group_num} has been sent off with their carts at time {env.now}.")
 
     def cart_return(self):
-        print(f"\nCarts are returning to the clubhouse at time {self.env.now}...")
+        print(f"Carts are returning to the clubhouse at time {self.env.now}...")
         while clubhouse.taken_carts:
             clubhouse.add_cart(clubhouse.taken_carts[0])  # Return the first cart
-            print(f"Returning cart. Available carts: {clubhouse.carts}. Taken carts: {clubhouse.taken_carts}")
+        #    print(f"Returning cart. Available carts: {clubhouse.carts}. Taken carts: {clubhouse.taken_carts}")
+
+
+
+    #def cart_return(self, env):
+     #   last_return_time = 0
+      #  return_gap = 5
+       # print(f"\nCarts are returning to the clubhouse at time {env.now}...")
+
+        #while clubhouse.taken_carts:
+         #   if env.now - last_return_time < return_gap:
+          #      wait_time = return_gap - (env.now - last_return_time)
+           #     print(f"Waiting {wait_time} minutes to return next group of carts...")
+            #    yield env.timeout(wait_time)
+        #clubhouse.add_cart(clubhouse.taken_carts[0])
+        #print(f"Returning cart. Available carts: {clubhouse.carts}. Taken carts: {clubhouse.taken_carts}")
+        #last_return_time = env.now
 
 
 if __name__ == "__main__":
