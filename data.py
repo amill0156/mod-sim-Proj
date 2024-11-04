faa379ee47334e4e964524bf5f7118e193cde922
amill0156 <ashton.miller82@gmail.com>
2024-11-03 21:15:20 -0500

    data study

--- data.py
+++ data.py
@@ -1,31 +1,18 @@
 import pandas as pd
+import re
 
-#data = []
+file_paths = ['coursedata.txt', 'coursedata2.txt', 'coursedata3.txt', 'coursedata4.txt', 'coursedata5.txt', 'coursedata6.txt', 'coursedata7.txt', 'coursedata8.txt', 'coursedata9.txt', 'coursedata10.txt']
 
-#with open('coursedata.txt', 'r') as file:
-#    for line in file:
-#        processed_line = line.strip().split()
-#        data.append(processed_line)
-#df = pd.DataFrame(data)
-#print(df)
+marshal_count = 0
+ahead_count = 0
+behind_count = 0
 
-#def parse_line(line):
-#    return line.strip().split()
-
-#with open('coursedata.txt', 'r') as file:
-#    data = [parse_line(line) for line in file]
-
-#df = pd.DataFrame(data)
-#print(df)
-
-search_word = 'marshal'
-
-matching_lines = []
-
-with open('coursedata.txt', 'r') as file:
-    for line in file:
-        if search_word in line:
-            matching_lines.append(line.strip())
-
-df = pd.DataFrame(matching_lines, columns=['Line'])
-print(df)
+for file_path in file_paths:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        marshal_count += len(re.findall(r"Marshal sent", content))
+        ahead_count += len(re.findall(r"ahead of pace", content))
+        behind_count += len(re.findall(r"behind pace", content))
+        print(marshal_count)
+        print(ahead_count)
+        print(behind_count)
